bc2ec5b3db143ef3068f91f5c8091568a38db219
Yaquaholic <richard@aqueouslife.co.uk>
2021-05-13 12:40:49 +0100

    Update Pagefile_utilisation.ps1

--- Monitors/Pagefile_utilisation.ps1
+++ Monitors/Pagefile_utilisation.ps1
@@ -1,7 +1,10 @@
-## SAM/APM Monitoring script
-## 2021 05 13
-## See: https://documentation.solarwinds.com/en/success_center/sam/content/sam-create-windows-powershell-script-monitor.htm
-## Limitation: SAM will only accept 10 results, so if you have 10+ pagefiles you will need to rethink this
+<# 
+.TITLE	Pagefle utilisation monitor
+.DATE	2021 05 13
+.USE	This script will retrieve the pagefile utilisation.
+.LIMITS   SAM will only accept 10 results, so if you have 10+ pagefiles you will need to rethink this
+.URL	https://documentation.solarwinds.com/en/success_center/sam/content/sam-create-windows-powershell-script-monitor.htm
+#>
 
 $pagefiles = Get-WmiObject -Class win32_PageFileUsage
 
