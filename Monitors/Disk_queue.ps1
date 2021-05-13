13703643fa2fa7d57269efabff3d515b9f6ea1a2
Yaquaholic <richard@aqueouslife.co.uk>
2021-05-13 12:38:23 +0100

    Update Disk_queue.ps1

--- Monitors/Disk_queue.ps1
+++ Monitors/Disk_queue.ps1
@@ -1,6 +1,11 @@
-## SAM(APM) PowerShell monitor for Disk Queues
-## 2021 05 13
-## _Total is a WMI catchall for all disks.
+<# 
+.TITLE	    Disk queue monitor
+.DATE		2021 05 13
+.USE		This script will retrieve the _Total disk queue length, anyhting above zero is noteworthy.
+            _Total is a WMI catchall for all disks. Check the output of `Get-WmiObject -Class win32_perfformatteddata_perfdisk_physicaldisk`
+            to see all the individual disks. 
+.URL		https://documentation.solarwinds.com/en/success_center/sam/content/sam-create-windows-powershell-script-monitor.htm
+#>
 
 $diskinfo = Get-WmiObject -Class win32_perfformatteddata_perfdisk_physicaldisk -Filter "Name LIKE '_Total'"
 $diskqueue = $diskinfo.AvgDisksecPerTransfer * $diskinfo.DiskTransfersPersec
